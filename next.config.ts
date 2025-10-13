cb37fea3b066ec3caf3b168fb4b22b714c1c714a
dem4ron <demaaron88@gmail.com>
2025-10-13 13:15:34 +0200

    Change back to turbopack

--- next.config.ts
+++ next.config.ts
@@ -4,20 +4,13 @@ const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: "@svgr/webpack",
-          options: {
-            dimensions: false,
-            titleProp: true,
-          },
-        },
-      ],
-    });
-    return config;
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
   },
 };
 
